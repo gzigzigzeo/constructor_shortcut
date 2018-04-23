6cda0606be8e3b8c893e5eb97121bb85a7f627ed
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-04-23 14:20:42 +0400

    Inspectors

--- lib/constructor_shortcut.rb
+++ lib/constructor_shortcut.rb
@@ -3,13 +3,24 @@ require "constructor_shortcut/version"
 # Generates anonymous module containing single class method
 module ConstructorShortcut
   # Used to set method name
+  # rubocop:disable Metrics/MethodLength
   def self.[](name = :call)
     @cache ||= {}
     @cache[name] ||=
       Module.new do
-        define_method name do |*args, **kwargs, &block|
-          new(*args, **kwargs, &block).public_send(name)
+        class << self
+          def name
+            "ConstructorShortcut[#{key}]"
+          end
+          alias_method :to_s, :name
+          alias_method :to_str, :name
+          alias_method :inspect, :name
+        end
+
+        define_method name do |*args, &block|
+          new(*args, &block).public_send(name)
         end
       end
   end
+  # rubocop:enable Metrics/MethodLength
 end
