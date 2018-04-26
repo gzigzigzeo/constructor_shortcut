67f9b9fc860c38dada9ec7f3548b8161aecf1228
Sergey Ponomarev <me@sergey-ponomarev.ru>
2018-04-26 13:57:23 +0300

    Fix module inspection, refactor tests a little bit

--- spec/constructor_shortcut_spec.rb
+++ spec/constructor_shortcut_spec.rb
@@ -1,5 +1,26 @@
-RSpec.describe TestConstructorShortcut do
-  it do
-    expect(described_class.call(5)).to eq(5)
+require "dry/initializer"
+
+RSpec.describe ConstructorShortcut do
+  let(:klass) do
+    Class.new do
+      extend Dry::Initializer
+      extend ConstructorShortcut[:call]
+
+      param :foo
+
+      def call
+        foo
+      end
+    end
+  end
+
+  it "creates shortcut" do
+    expect(klass.call(5)).to eq(5)
+  end
+
+  it "shows proper ancestors for singleton class" do
+    expect(klass.singleton_class.ancestors).to include(
+      ConstructorShortcut[:call]
+    )
   end
 end
