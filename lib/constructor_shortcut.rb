24aabecde8ddc557131e37a27879c964e5fabeb1
Viktor Sokolov <gzigzigzeo@gmail.com>
2019-09-03 02:03:29 +0300

    Get rid of unused cache module anymore

--- lib/constructor_shortcut.rb
+++ lib/constructor_shortcut.rb
@@ -1,11 +1,32 @@
 require "constructor_shortcut/version"
 require "constructor_shortcut/cache"
 
+# rubocop:disable Metrics/MethodLength
+
 # Generates anonymous module containing single class method
 module ConstructorShortcut
   # Used to set method name
   def self.[](name = :call, class_name = :call)
-    Cache.resolve(name, class_name)
+    @cache ||= {}
+    key = "#{name.inspect}, #{class_name.inspect}".freeze
+
+    @cache[key] ||=
+      Module.new do
+        @key = key
+
+        class << self
+          def name
+            "ConstructorShortcut[#{@key}]"
+          end
+          alias_method :to_s, :name
+          alias_method :to_str, :name
+          alias_method :inspect, :name
+        end
+
+        define_method class_name do |*args, &block|
+          new(*args, &block).public_send(name)
+        end
+      end
   end
 
   def call(*args, &block)
